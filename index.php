4705aaaf400db17ee727f6f3463ae6ca9ea37861
Jehad Alqurashi <jehad@Jehads-MacBook-Pro.local>
2021-10-30 17:37:20 +0300

    test

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+        <head>
+                <title>
+                        CTF Challange
+                </title>
+                <link rel="stylesheet" href="css/index.css">
+                <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+
+        </head>
+
+        <body>
+                <div class="head">
+                <h1>CTF CHALLANGE </h1><i style="font-size:24px" class="fas fa-flag"></i>
+                </div>
+        </body>
+
+</html>
