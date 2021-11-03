8cfc863e56cdf7c9188e3368f7de418c272fccfb
Jehad Alqurashi <anmar-v7@hotmail.com>
2021-11-04 01:04:37 +0300

    dd

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
         <head>
                 <title>
-                        CTF Challange
+                        CTF Challenge
                 </title>
                 <link rel="stylesheet" href="css/index.css">
                 <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
@@ -9,16 +9,45 @@
         </head>
 
         <body>
+        <img src="img/banner.png">
                 <div class="head">
-                <h1>CTF CHALLANGE </h1><i style="font-size:24px" class="fas fa-flag"></i>
+                <h1>CTF CHALLENGES </h1>
                 </div>
-                <hr>
+                <h3 class="head2">Categories</h3>
                 <div class="cate">
-                        <h3 class="head2">Categories</h3>
-                        <div>
-                                <h4>Web</h4>
-                                
+                    <i style="font-size:24px;position: absolute;color:black;left: 50%" class="fas fa-flag"></i>
+                        <div class="web">
+                                <h4 style="background: #fdfcfc;border:1px solid black;margin-top: 0;color:black;height: 70px"><span style="position: relative ;top:20px;font-weight: bold">General</span></h4>
+                                <ul class="webOrder">
+                                    <li><a href="web/" >LFI</a></li>
+                                    <li><a href="web/" >LFI</a></li>
+                                    <li><a href="web/" >LFI</a></li>
+                                </ul>
                         </div>
+                    <div class="web">
+                        <h4 style="background: #fdfcfc;border:1px solid black;margin-top: 0;color:black;height: 70px"><span style="position: relative ;top:20px;font-weight: bold">Web</span></h4>
+                        <ul class="webOrder">
+                            <li><a href="web/">LFI</a></li>
+                            <li><a href="web/" >LFI</a></li>
+                            <li><a href="web/">LFI</a></li>
+                        </ul>
+                    </div>
+                    <div class="web">
+                        <h4 style="background: #fdfcfc;border:1px solid black;margin-top: 0;color:black;height: 70px"><span style="position: relative ;top:20px;font-weight: bold">Steganography</span></h4>
+                        <ul class="webOrder">
+                            <li><a href="web/">LFI</a></li>
+                            <li><a href="web/" >LFI</a></li>
+                            <li><a href="web/" >LFI</a></li>
+                        </ul>
+                    </div>
+                    <div class="web">
+                        <h4 style="background: #fdfcfc;border:1px solid black;margin-top: 0;color:black;height: 70px"><span style="position: relative ;top:20px;font-weight: bold">Network</span></h4>
+                        <ul class="webOrder">
+                            <li><a href="web/" >LFI</a></li>
+                            <li><a href="web/">LFI</a></li>
+                            <li><a href="web/">LFI</a></li>
+                        </ul>
+                    </div>
                 </div>
                
         </body>
