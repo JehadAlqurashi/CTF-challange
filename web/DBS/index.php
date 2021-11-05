374c42024dc3fb0690ba45c6f49d88541479db2f
Jehad Alqurashi <jehad@Jehads-MacBook-Pro.local>
2021-11-05 16:55:28 +0300

    test

---
+++ web/DBS/index.php
@@ -0,0 +1,30 @@
+<?php
+$connect =new mysqli("localhost","root","","ctf");
+if($connect->error){
+    echo $connect->error;
+}
+if($_SERVER['REQUEST_METHOD']=="POST"){
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $check = $connect->query("select * from users where email='$email' and password='$password' ");
+    if(!$check->num_rows){
+        $fail =  "Email Or Password is Wrong";
+    }
+}
+
+
+
+?>
+<link rel="stylesheet" href="DBS.css">
+<div class="login">
+    
+  <div class="form">
+  <?php echo $fail; ?>
+    <form class="login-form" action="" method="post">
+      <span class="material-icons">lock</span>
+      <input name="email" type="text" placeholder="email" required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" required/>
+      <input name="password" type="password" placeholder="password" required />
+      <button>login</button>
+    </form>  
+  </div>
+</div>
