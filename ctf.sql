560e6cc4eb3c696eced18be8a59ca554eceb991a
Jehad Alqurashi <anmar-v7@hotmail.com>
2021-11-10 03:31:50 +0300

    SQL File

--- ctf.sql
+++ ctf.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Nov 05, 2021 at 06:20 PM
--- Server version: 10.4.21-MariaDB
--- PHP Version: 8.0.11
+-- Host: 127.0.0.1
+-- Generation Time: Nov 10, 2021 at 01:30 AM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 8.0.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -48,7 +48,7 @@ CREATE TABLE `points` (
   `id` int(1) NOT NULL,
   `flag` varchar(200) NOT NULL,
   `name` varchar(30) NOT NULL,
-  `count` set('0','1') NOT NULL DEFAULT '0',
+  `count` set('0','1') NOT NULL,
   `point` int(11) NOT NULL,
   `type` set('general','web','network','stegno') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -63,7 +63,14 @@ INSERT INTO `points` (`id`, `flag`, `name`, `count`, `point`, `type`) VALUES
 (3, 'Flag{431bc85b812bd8bcac81bdd27429a12d}', 'humble', '0', 100, 'web'),
 (4, 'Flag{85afaab5f3b6a638269e33d12da2fedf}', 'dbs', '0', 80, 'web'),
 (5, 'Flag{8b373710bcf876edd91f281e50ed58ab}', 'history', '0', 20, 'general'),
-(6, 'Flag{9678a98e2905327e855263bb52ade7e0}', 'wnnacry', '0', 20, 'general');
+(6, 'Flag{9678a98e2905327e855263bb52ade7e0}', 'wnnacry', '0', 20, 'general'),
+(7, 'Flag{e0aca2fe8231010480c521fa93bc7ee6}', 'Close_Look', '0', 20, 'general'),
+(8, 'Flag{ec63d66a5179177893ab7e40f1e662fa}', 'Intercept', '0', 20, 'general'),
+(9, 'Flag{ad13e3e85208780288a2389c791b5c03}', 'Hashes', '0', 150, 'web'),
+(10, 'Flag{b1f4f9a523e36fd969f4573e25af4540}', 'Cat', '0', 100, 'stegno'),
+(11, 'Flag{36cd38f49b9afa08222c0dc9ebfe35eb}', 'Source', '0', 50, 'web'),
+(12, 'Flag{9b8f27ea39bdc8d44687362fee88f9fc}', 'Klega', '0', 100, 'stegno'),
+(13, 'Flag{c0af77cf8294ff93a5cdb2963ca9f038}', 'Colors', '0', 120, 'stegno');
 
 -- --------------------------------------------------------
 
@@ -108,7 +115,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `points`
 --
 ALTER TABLE `points`
-  MODIFY `id` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `users`
